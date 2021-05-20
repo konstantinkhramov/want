a60ef6e418b5ddd58a9f728016c06617dea85ca2
kostya <khramovkonstantin94@gmail.ru>
2021-05-20 17:47:33 +0300

    added new migrate for book change fields.

--- django_want/author/models.py
+++ django_want/author/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Author(models.Model):
     """Модель описывающая автора книги"""
-
     name = models.CharField(max_length=140)
     last_name = models.CharField(max_length=140)
     middle_name = models.CharField(max_length=140, null=True)
--- django_want/books/models.py
+++ django_want/books/models.py
@@ -2,14 +2,16 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 # Create your models here.
+from author.models import Author
 from users.models import User
 
 
 class Books(models.Model):
     """Модель описывающая книги"""
-    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    author_id = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     book_caption = models.CharField(max_length=225)
-    author_book_name = models.CharField(max_length=225)
     rating = models.IntegerField(validators=[MinValueValidator(0),
                                              MaxValueValidator(5)])
     create_date = models.DateTimeField(auto_now_add=True)
