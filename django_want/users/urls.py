9bc19b5c25b4fbe7d404d46bf19f9f690e262540
kostya <khramovkonstantin94@gmail.ru>
2021-10-26 21:07:35 +0300

    change url for user login and register

--- django_want/users/urls.py
+++ django_want/users/urls.py
@@ -5,8 +5,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.UserRegister.as_view(), name='user_register'),
-    path('login/', views.UserLogin.as_view(), name='user_login'),
+    path('users/register/', views.UserRegister.as_view(), name='user_register'),
+    path('users/login/', views.UserLogin.as_view(), name='user_login'),
     path('users/', views.UserList.as_view(), name='users'),
     path('users/<pk>/', views.UserDetails.as_view(), name='user'),
     path('group/', views.GroupList.as_view(), name='group')
